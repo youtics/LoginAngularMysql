d3a25aa003dbe4fe942a2b8db6e9f46efe22212c
youtics <youtics@gmail.com>
2022-07-31 14:23:36 -0300

    Version 2 con el front-end avanzado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthModule } from './auth/auth/auth.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
